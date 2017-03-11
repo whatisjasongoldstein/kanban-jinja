8c00bfa20bfd6d33ad710c68651a410dd1a2f2bb
Jason Goldstein <jason@betheshoe.com>
2017-03-11 13:46:13 -0500

    Build CSS too

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,8 +4,48 @@ import gulp from 'gulp';
 import nunjucks from 'gulp-nunjucks';
 import concat from 'gulp-concat';
 import child_process from 'child_process';
+import plumber from 'gulp-plumber';
+import autoprefixer from 'gulp-autoprefixer';
+import rename from 'gulp-rename';
+import notify from 'gulp-notify';
+import sass from 'gulp-sass';
+import sourcemaps from 'gulp-sourcemaps';
 
 const outdir = "assets/";
+
+
+var onError = function(err) {
+    notify.onError({
+        title:    "Gulp",
+        subtitle: "Failure!",
+        message:  "Error: <%= error.message %>",
+        sound:    "Beep"
+    })(err);
+
+    this.emit('end');
+};
+
+
+gulp.task('css', function() {
+    return gulp.src('frontend/css/app.scss')
+      .pipe(plumber({errorHandler: onError}))
+      .pipe(sourcemaps.init())
+      .pipe(sass({
+          follow: true,
+          outputStyle: "compressed"
+      }))
+      .pipe(autoprefixer({
+          browsers: ['last 2 versions'],
+          cascade: false
+      }))
+      .pipe(sourcemaps.write('./maps'))
+      .pipe(rename(function(path){
+          path.extname = ".min" + path.extname;
+          return path;
+      }))
+      .pipe(gulp.dest(outdir))
+});
+
 
 gulp.task("js", ["templates"], () => {
   return gulp.src([
@@ -30,9 +70,10 @@ gulp.task("templates", () => {
 })
 
 
-gulp.task('dev', function() {
-    gulp.watch('todo/**/*.js', ['js']);
-    gulp.watch('todo/templates/*.njk', ['js']);
+gulp.task('watch', function() {
+    gulp.watch('frontend/**/*.js', ['js']);
+    gulp.watch('frontend/templates/components/*.html', ['js']);
+    gulp.watch('frontend/**/*.scss', ['css']);
 });
 
-gulp.task('default', ['js', ]);
+gulp.task('default', ['js', 'css']);
